180f79982ce9f9376670e5dc6b12254040d621f3
shaunakk <epicshaunak@gmail.com>
2016-04-09 13:21:36 -0700

    date

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
 
+$(document).ready(function(){
+
+	$("#approveBook").hide()
+	$("#request").click(function(){
+		$("#request").parent().addClass("active")
+		$("#approve").parent().removeClass("active")
+		$("#requestBook").show()
+		$("#approveBook").hide()
+	})
+	$("#approve").click(function(){
+		$("#approve").parent().addClass("active")
+		$("#request").parent().removeClass("active")
+		$("#requestBook").hide()
+		$("#approveBook").show()
+	})
+	$("#makeRequest").click(function(){
+		book = $("#bookName").val().trim().replace(/ /g, "+");
+		var path = 'http://localhost:8080?bname=' + book;
+		$.ajax({
+			method:'GET',
+			url: path,
+			success: function(data){
+				$(".bookPrice").html("Book Price: " + data)
+			}
+		})
+	});
+})
